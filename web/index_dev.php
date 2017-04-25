80c8cd46da39f242b47db223cdcfe7bfb25a7c0c
Ronan Chilvers <ronan@d3r.com>
2017-04-25 08:54:03 +0100

    Fix static assets when using php builtin server

--- web/index_dev.php
+++ web/index_dev.php
@@ -3,6 +3,13 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 
 use App\Application;
 
+if (php_sapi_name() == "cli-server") {
+    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    if ('/' !== $path && file_exists(__DIR__ . $path)) {
+        return false;
+    }
+}
+
 $app = include(__DIR__ . '/../app/config/app.php');
 include(__DIR__ . '/../app/config/dev.php');
 $app->run();
