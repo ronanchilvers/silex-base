1f9d93f5d8bc433b84c06d45dc65b4d36e33b020
Ronan Chilvers <ronan@d3r.com>
2018-01-17 08:46:54 +0000

    Add some controller shortcuts

--- src/Controller.php
+++ src/Controller.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use App\Application;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 abstract class Controller
 {
@@ -65,6 +68,70 @@ abstract class Controller
     }
 
     /**
+     * Render a JSON response
+     *
+     * @param $data
+     * @return Symfony\Component\HttpFoundation\JsonResponse
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function json($data)
+    {
+        return new JsonResponse(
+            $data
+        );
+    }
+
+    /**
+     * Redirect to a named route
+     *
+     * @param string $routeName
+     * @param int $status
+     * @return Symfony\Component\HttpFoundation\RedirectResponse
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    protected function redirectToRoute($routeName, $parameters = [], $status = 302)
+    {
+        $generator = $this->app()['url_generator'];
+        try {
+            $url = $generator->generate(
+                $routeName,
+                $parameters
+            );
+        } catch (RouteNotFoundException $ex) {
+            $url = $routeName;
+        }
+
+        return $this->redirect($url, $status);
+    }
+
+    /**
+     * Redirect to the previous page
+     *
+     * @param string $defaultRouteName
+     * @return Symfony\Component\HttpFoundation\RedirectResponse
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    protected function redirectToPrevious(
+        Request $request,
+        $defaultRouteName,
+        $parameters = []
+    ) {
+        $referer = $request->headers->get('referer');
+        if (empty($referer)) {
+            return $this->redirectToRoute(
+                $defaultRouteName,
+                $parameters,
+                302
+            );
+        }
+
+        return $this->redirect(
+            $referer,
+            302
+        );
+    }
+
+    /**
      * Redirect to a url
      *
      * @param  string $url
@@ -75,5 +142,20 @@ abstract class Controller
     protected function redirect($url, $status = 302)
     {
         return $this->app()->redirect($url, $status);
+    }
+
+    /**
+     * Set a flash message
+     *
+     * @param string $type
+     * @param string $message
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    protected function flash($type, $message)
+    {
+        $this->app()['session']->getFlashBag()->add(
+            $type,
+            $message
+        );
     }
 }
